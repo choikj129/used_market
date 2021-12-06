97df5533c5fcc9dfb86ee8ed90f624c26e8a697e
choikj129 <ckj9829@naver.com>
2021-12-06 19:48:42 +0900

    2021-12-06
    이미지 파일 저장

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const fs = require("fs");
 const Crypto = require("crypto");
 const data = JSON.parse(fs.readFileSync(__dirname + '/db.json'));
 const key = data.keypair
+const bodyParser = require("body-parser")
+const multer = require("multer")
+const path = require("path")
 
 const db = mysql.createConnection({
     host : data.host,
@@ -14,6 +17,20 @@ const db = mysql.createConnection({
     password : data.password,
     database : data.database    
 })
+
+// 이미지 업로드
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+var storage = multer.diskStorage({
+    destination : function (req, file, callback){
+        callback(null, "public/img")
+    },
+    filename : function(req, file, callback){
+        const ext = path.extname(file.originalname)
+        callback(null, path.basename(file.originalname, ext)+"-" +Date.now()+ext)
+    }
+})
+var upload = multer({storage : storage})
 
 app.set("views", __dirname+"/views");
 app.set("view engine", "ejs")
@@ -43,10 +60,17 @@ app.get("/", function(req, res){
 })
 
 app.get("/index", function(req, res){
-    res.render("index.ejs")
+    res.render("index")
 })
 
 app.get("/main", function(req, res){
-    res.render("main.ejs")
+    res.render("main")
 })
 
+app.get("/regist",function(req, res){
+    !req.session.log ? res.redirect("/") : res.render("regist")
+})
+
+app.post("/regist/commit", upload.array("images"), function(req,res){
+    console.log(req.files)
+})
